7cab468fb9083af79b1bc6744ab2d09d1b442220
DavMLPath <davit.minasyan.98@mail.ru>
2023-04-14 16:28:36 +0400

    add sarimax to git

---
+++ Sarimax_gpt.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+
